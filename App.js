a68ff7d9ebb798d8465da30a5eceea652cc52673
emiliocramer <emilio.cramer101@gmail.com>
2023-07-07 01:58:31 -0400

    sd

--- App.js
+++ App.js
@@ -1,29 +1,31 @@
 const express = require('express');
 const mongodb = require('mongodb');
 const app = express();
-
-const MongoClient = mongodb.MongoClient;
-
-const username = encodeURIComponent('rapworldfilms');
+const { MongoClient, ServerApiVersion } = require('mongodb');
 const password = encodeURIComponent('fon@2124#dds');
-const clusterName = 'rap-world-db';
-const dbName = 'rap-world-db';
 
-const url = `mongodb+srv://rapworldfilms:${password}@rap-world-db.fsu4qy1.mongodb.net/?retryWrites=true&w=majority`;
-
-// Create a new MongoClient
-const client = new MongoClient(url);
-
-// Use connect method to connect to the server
-client.connect(function(err) {
-    if (err) throw err;
-    console.log("Connected successfully to server");
-
-    const db = client.db(dbName);
-
-    // Close the connection
-    client.close();
+const uri = "mongodb+srv://rapworldfilms:<password>@rap-world-db.fsu4qy1.mongodb.net/?retryWrites=true&w=majority";
+// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+const client = new MongoClient(uri, {
+    serverApi: {
+        version: ServerApiVersion.v1,
+        strict: true,
+        deprecationErrors: true,
+    }
 });
+async function run() {
+    try {
+        // Connect the client to the server	(optional starting in v4.7)
+        await client.connect();
+        // Send a ping to confirm a successful connection
+        await client.db("admin").command({ ping: 1 });
+        console.log("Pinged your deployment. You successfully connected to MongoDB!");
+    } finally {
+        // Ensures that the client will close when you finish/error
+        await client.close();
+    }
+}
+run().catch(console.dir);
 
 app.get('/', (req, res) => {
     res.send('Hello World!')
