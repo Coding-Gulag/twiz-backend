e4cd1a1e5993c0d601664c4a43572eb501069a2e
emiliocramer <emilio.cramer101@gmail.com>
2023-07-07 01:59:57 -0400

    sdf

--- App.js
+++ App.js
@@ -4,7 +4,7 @@ const app = express();
 const { MongoClient, ServerApiVersion } = require('mongodb');
 const password = encodeURIComponent('fon@2124#dds');
 
-const uri = "mongodb+srv://rapworldfilms:<password>@rap-world-db.fsu4qy1.mongodb.net/?retryWrites=true&w=majority";
+const uri = `mongodb+srv://rapworldfilms:${password}@rap-world-db.fsu4qy1.mongodb.net/?retryWrites=true&w=majority`;
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
 const client = new MongoClient(uri, {
     serverApi: {
